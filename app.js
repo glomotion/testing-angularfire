f14e80efd80264111d152d1c874d986cc28686b5
Tim Paul <timpaul@google.com>
2015-01-07 16:54:34 +1100

    adding ui to allow signin from multiple services...

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var app = angular.module("sampleApp", ["firebase"]);
 
 app.controller("SampleCtrl", function($scope, $firebase, $firebaseAuth) {
-  var ref = new Firebase("https://crackling-inferno-3267.firebaseio.com/data");
-  var sync = $firebase(ref);
 
-  // download the data into a local object
-  var syncObject = sync.$asObject();
+	var ref = new Firebase("https://crackling-inferno-3267.firebaseio.com/data");
+	var sync = $firebase(ref);
 
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+	// download the data into a local object
+	var syncObject = sync.$asObject();
 
-  var auth = $firebaseAuth(ref);
-  auth.$authWithOAuthPopup("google").then(function(authData) {
-    console.log("Logged in as:", authData.uid);
-  }).catch(function(error) {
-    console.error("Authentication failed: ", error);
-  });
+	// synchronize the object with a three-way data binding
+	// click on `index.html` above to see it used in the DOM!
+	syncObject.$bindTo($scope, "data");
+	
+	$scope.loggedUser = 'Logging in...';
+	
+	$scope.signinWith = function(serviceName) {
+		
+		var auth = $firebaseAuth(ref);
+		
+		auth.$authWithOAuthPopup(serviceName).then(function(authData) {
+			console.log("Logged in as:", authData.uid);
+			$scope.loggedUser = authData.uid;
+		}).catch(function(error) {
+			console.error("Authentication failed: ", error);
+			$scope.loggedUser = 'Auth failed: ' + error;
+		});
+	}
+
 });
