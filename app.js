28d2294ba2688bdf43d6a216e49d94070777742b
tim <tim@glo.id.au>
2015-01-13 15:41:47 +1100

    refining look and feel of the RTE

--- app.js
+++ app.js
@@ -27,18 +27,32 @@ app.controller("SampleCtrl", function($scope, $firebase, $firebaseAuth) {
 	
 	$scope.loggedUser = 'Logging in...';
 	
-	$scope.signInWith = function(serviceName) {
-		
-		var auth = $firebaseAuth(ref);
-		
-		auth.$authWithOAuthPopup(serviceName).then(function(authData) {
-			console.log("Logged in as:", authData.uid);
-			$scope.loggedUser = authData.uid;
-			$scope.fullUserData = authData;
-		}).catch(function(error) {
-			console.error("Authentication failed: ", error);
-			$scope.loggedUser = 'Auth failed: ' + error;
-		});
-	}
 
-});
+})
+
+
+.config(['$provide', function($provide){
+    // this demonstrates how to register a new tool and add it to the default toolbar
+    $provide.decorator('taOptions', ['$delegate', function(taOptions){
+        // $delegate is the taOptions we are decorating
+        // here we override the default toolbars and classes specified in taOptions.
+        taOptions.toolbar = [
+            ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+            ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+            ['justifyLeft','justifyCenter','justifyRight'],
+            ['html', 'insertImage', 'insertLink']
+        ];
+        taOptions.classes = {
+            focussed: 'focussed',
+            toolbar: 'btn-toolbar',
+            toolbarGroup: 'btn-group',
+            toolbarButton: 'btn btn-default',
+            toolbarButtonActive: 'active',
+            disabled: 'disabled',
+            textEditor: 'form-control',
+            htmlEditor: 'form-control'
+        };
+        return taOptions; // whatever you return will be the taOptions
+    }]);
+
+}])
