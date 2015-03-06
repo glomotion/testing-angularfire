1a23e7ef1172e2398ce859995cbf7113b2ec5a52
Tim Paul <timpaul@google.com>
2015-03-06 15:51:36 +1100

    code formatting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = angular.module("sampleApp", ["firebase"]);
 
 app.controller("SampleCtrl", function($scope, $firebase, $firebaseAuth) {
 
+	// 3 WAY DATA SYNC STUFF:
 	var ref = new Firebase("//crackling-inferno-3267.firebaseio.com/data");
 	var sync = $firebase(ref);
 
@@ -11,17 +12,18 @@ app.controller("SampleCtrl", function($scope, $firebase, $firebaseAuth) {
 	// synchronize the object with a three-way data binding
 	// click on `index.html` above to see it used in the DOM!
 	syncObject.$bindTo($scope, "data");
-	
-	$scope.loggedUser = 'Logging in...';
+
 
 	$scope.$watch('data',function(n,o){
 		console.log(n);
 	})
-	
+
+	// AUTH RELATED STUFF:
+	$scope.loggedUser = '(Not logged in)';
+
+	// SIMPLE O-AUTH FLOW TO CALL UPON CLICK:
 	$scope.signInWith = function(serviceName) {
-		
 		var auth = $firebaseAuth(ref);
-		
 		auth.$authWithOAuthPopup(serviceName).then(function(authData) {
 			console.log("Logged in as:", authData.uid);
 			$scope.loggedUser = authData.uid;
